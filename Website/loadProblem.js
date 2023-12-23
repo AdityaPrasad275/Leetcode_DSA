f10b1df8e6dbc65800f4b0ee3fbf72b95466a57e
Aditya Prasad <adityanprasad275@gmail.com>
2023-12-23 13:25:47 +0530

    another minor error

--- Website/loadProblem.js
+++ Website/loadProblem.js
@@ -1,7 +1,7 @@
 // loadProblem.js
 async function loadProblem(url) {
   try {
-    const response = await fetch(`website/${url}`);
+    const response = await fetch(`${url}`);
     const markdown = await response.text();
     const html = marked.parse(markdown);
     document.getElementById('problem-content').innerHTML = html;
